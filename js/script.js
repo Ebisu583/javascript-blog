347c1986ec3e06443b2a0242e56369c2b84c7466
Radoslaw Tyszkiewicz <ebisu.poland@gmail.com>
2021-04-05 13:46:06 +0900

    Create article display functionality

--- js/script.js
+++ js/script.js
@@ -3,13 +3,26 @@
 //     console.log('links:', links);
 //   })
 const titleClickHandler = function(event){
-    console.log(event.target);
+    event.preventDefault();
+    console.log(event.currentTarget.getAttribute(`href`));
     //Remove the class active from all links
+    const activeLinks = document.querySelectorAll(`.titles a.active`)
+    for (let activeLink of activeLinks){
+        activeLink.classList.remove("active")
+    }
     //Add class active to the clicked link
+    event.currentTarget.classList.add("active");
     //Remove the class active from all articles
+    const activeArticles = document.querySelectorAll(".post.active")
+    for (let activeArticle of activeArticles){
+        activeArticle.classList.remove("active");
+    }
     // get 'href' attribute from the clicked link
+    const linkHref = event.currentTarget.getAttribute(`href`)
     //Select the article corresponding to the link
+    const selectedArticle = document.querySelector(linkHref)
     //Add class active to the clicked article
+    selectedArticle.classList.add("active");
 }
 
 const links = document.querySelectorAll('.titles a');

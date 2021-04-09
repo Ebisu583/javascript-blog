61c0fa5412912ebf6e4ea10df2111a2875df6aa7
Radoslaw Tyszkiewicz <ebisu.poland@gmail.com>
2021-04-09 18:34:44 +0900

    Generate title links by JS

--- js/script.js
+++ js/script.js
@@ -25,8 +25,51 @@ const titleClickHandler = function(event){
     selectedArticle.classList.add("active");
 }
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
 
-for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+function generateTitleLinks(){
+    
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = "";
+    
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+    for(let article of articles){
+        /* get the article id */
+        const articleId = article.getAttribute(`id`);
+        console.log(articleId);
+        /* find the title element */
+        /* get the title from the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        console.log(articleTitle);
+        /* create HTML of the link */
+        const linkHtml = `<li><a href="#` + articleId + `"><span>`+ articleTitle + `</span></a></li>`
+        console.log(linkHtml);
+        titleList.insertAdjacentHTML("beforeend", linkHtml);
+        
+        const links = document.querySelectorAll('.titles a');
+        
+        for(let link of links){
+            link.addEventListener('click', titleClickHandler);
+        }
+    }
+
+
+
+
+
+    /* insert link into titleList */
+
 }
+
+generateTitleLinks();
+
+
+
+
+
+
+
